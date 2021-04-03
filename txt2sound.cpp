56bec59464faf4c12aa6ecbe35c08c8785c87aff
woahdas <78304648+woahdas@users.noreply.github.com>
2021-04-03 10:17:05 +0100

    Update txt2sound.cpp

--- txt2sound.cpp
+++ txt2sound.cpp
@@ -2,7 +2,7 @@
 #include <stdio>
 #include <Windows.h>
 
-using namespace std
+using namespace std;
 
 int main()
 {
@@ -10,13 +10,13 @@ int main()
 
 	cout << "What's the file you want to transfer?";
 	cin >> targetfile;
-	cout << "Is " targetfile << "correct? If not, please interrupt the program";
+	cout << "Is " targetfile << "correct? Do Ctrl+C if it isn't";
 	Sleep(5000);
 
 	bool fileavailable = ofstream myfile && myfile.open(targetfile) && myfile.close(); //This sets it to false if it can't open the file (it can't open the file if it can't find it)
 	if (fileavailable = false)
 	{
-		cout << "The program can't seem to find the file requested";
+		cout << "The file cannot be found";
 	}
 	for (int i = 0; int i < 1000000000; i++)
 	{
@@ -30,7 +30,6 @@ int main()
 			}
 			if (targetfile.eof())
 			{
-				cout << "Finished!";
 				break;
 			}
 		}
